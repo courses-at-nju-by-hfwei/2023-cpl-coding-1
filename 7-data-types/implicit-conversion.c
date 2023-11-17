b105d3d95d82ced8cad17691033d24f7137dfe9a
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2023-11-17 22:32:10 +0800

    7-data-types: +code after class

--- 7-data-types/implicit-conversion.c
+++ 7-data-types/implicit-conversion.c
@@ -10,12 +10,17 @@ double SquareDouble(double num);
 
 int main() {
   // narrowing conversion (still in the range)
+  int i = 3.14159;
 
   // out of the range: undefined behavior!!!
+  int j = UINT_MAX;
 
   // arguments; narrowing conversion
+  double k = 3.14159;
+  SquareInt(k);
 
   // return value; narrowing conversion
+  int val = SquareDouble(k);
 
   // from int to float; narrowing conversion
   int big = 1234567890;
